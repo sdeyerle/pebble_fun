2d9a179fb7ca7791a5723d81a64fe97e37cec807
Shaver Deyerle <sdeyerle@vt.edu>
2014-01-31 23:19:39 -0800

    Added the precompiled ARM toolchain for Mac OS X

---
+++ PebbleSDK-2.0-BETA7/arm-cs-tools/lib/gcc/arm-none-eabi/4.7.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.7-2012.09/configure --prefix=/Users/thomas/pebble-dev/arm-cs-tools --with-pkgversion='32-bit ARM EABI Toolchain JBS-2012.09-63-v2011.09-69-20-gfef160a' --with-bugurl=https://github.com/jsnyder/arm-eabi-toolchain --target=arm-none-eabi --enable-languages=c,c++ --with-gnu-ld --with-gnu-as --with-newlib --disable-nls --disable-libssp --disable-shared --enable-threads --with-headers=yes --disable-libmudflap --disable-libgomp --enable-lto --disable-libstdcxx-pch --enable-poison-system-directories --with-sysroot=/Users/thomas/pebble-dev/arm-cs-tools/arm-none-eabi --with-build-time-tools=/Users/thomas/pebble-dev/arm-cs-tools/arm-none-eabi/bin --with-gmp=/Users/thomas/pebble-dev/arm-cs-tools --with-mpfr=/Users/thomas/pebble-dev/arm-cs-tools --with-mpc=/Users/thomas/pebble-dev/arm-cs-tools --enable-extra-sgxxlite-multilibs --with-specs='%{save-temps: -fverbose-asm} -D__CS_SOURCERYGXX_MAJ__=2012 -D__CS_SOURCERYGXX_MIN__=09 -D__CS_SOURCERYGXX_REV__=63 %{O2:%{!fno-remove-local-statics: -fremove-local-statics}} %{O*:%{O|O0|O1|O2|Os:;:%{!fno-remove-local-statics: -fremove-local-statics}}}' --with-gmp=/Users/thomas/work/arm-eabi-toolchain/build/libs/ --with-mpfr=/Users/thomas/work/arm-eabi-toolchain/build/libs/ --with-mpc=/Users/thomas/work/arm-eabi-toolchain/build/libs/";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
